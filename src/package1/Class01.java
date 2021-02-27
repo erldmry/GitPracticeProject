eb5e4aef3b001b76720175a7612d4b87f92fd34b
Erol D <eroldemirayy@gmail.com>
2021-02-27 20:41:18 +0100

    third commit

--- src/package1/Class01.java
+++ src/package1/Class01.java
@@ -7,6 +7,11 @@ public class Class01 {
 		
 		String name = "Hello Git";
 		System.out.println(name);
+		
+		String name1 = "Hello Git again";
+		System.out.println(name1);		
+		
+		// I add some info as comment that can i see on the git
 
 	}
 
